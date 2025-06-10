7ad22101f6c80021e80946b608a2ec07603d0eeb
Lab PC <uavforge.uci.sdp@gmail.com>
2025-06-10 15:25:08 -0700

    fill out stub more

--- visual_odometry.py
+++ visual_odometry.py
@@ -12,6 +12,14 @@ import requests
 from lightglue import LightGlue, SuperPoint
 from lightglue.utils import load_image, rbd
 from scipy.spatial.transform import Rotation
+import gtsam
+from gtsam import symbol_shorthand
+
+from pyLSHash import LSHash # https://github.com/guofei9987/pyLSHash/tree/main
+
+X = symbol_shorthand.X
+L = symbol_shorthand.L
+
 
 CV_WORLD_FRAME_TO_WORLD_FRAME_MAT_ROTATION = np.array(
     [[0, 0, 1], [-1, 0, 0], [0, 1, 0]], dtype=np.float32
@@ -29,6 +37,7 @@ CV_WORLD_FRAME_TO_WORLD_FRAME_MAT = (
 )
 
 CV_TO_WORLD_ROT = Rotation.from_matrix(CV_WORLD_FRAME_TO_WORLD_FRAME_MAT)
+
 
 
 def cv_to_pose(rvec, tvec):
@@ -139,9 +148,82 @@ def compute_relative_pose(E, points0, points1) -> tuple[np.ndarray, Rotation]:
     pose_t, pose_r = cv_to_pose(rvec, tvec)
     return pose_t, pose_r
 
+measurement_noise = gtsam.noiseModel.Isotropic.Sigma(2, 1.0)  # one pixel in u and v
+
 def add_vo_factors(
-    g,
+    g: gtsam.NonlinearFactorGraph,
     data: SyncedDatum,
-    variables: tuple[str,str],
-    keypoint_data
-)
+    initial_values: gtsam.Values,
+    pose_variable_indices: tuple[int,int],
+    keypoint_data: LSHash
+):
+    '''
+    1: compute keypoints matches and relative pose between images
+    2. get pose estimate of first image from graph
+    3. look up keypoint descriptors in the map
+        4. for keypoints that do not exist in the map:
+            a. use pose estimate + relative pose to triangulate keypoint
+            b. add a new landmark variable to the map 
+            c. add factors between the old and new poses, and that new landmark variable
+        5. for keypoints that exist in the map:
+            a. add factors between the new pose and those landmark variables
+    
+    map data structure capabilities:
+    
+    key: descriptor
+    value: gtsam Position3 variable
+
+    mutates `g` and `keypoint_data` to add new factors and landmarks
+    '''
+
+    # 1:
+    points0, points1, descriptors, E = compute_matches(data.img_left_start, data.img_right_start)
+    t, r = compute_relative_pose(E, points0, points1)
+
+    # 2:
+    cam0_pose = initial_values.atPose3(X(pose_variable_indices[0]))
+
+    # 3
+    for p0, p1, d in zip(points0, points1, descriptors):
+        matches = keypoint_data.query(d.tolist())
+        # 4
+        if len(matches)==0: # keypoint does not exist in map
+            index = len()
+            # add (d, index) to hash table
+            landmark = L(index)
+            g.push_back(GenericProjectionFactorCal3_S2(p0, measurement_noise, X(pose_variable_indices[0]), L(index)))
+            g.push_back(GenericProjectionFactorCal3_S2(p1, measurement_noise, X(pose_variable_indices[1]), L(index)))
+        # 5
+        else: # get closest match and work with it
+            index = matches[0][0][1]
+            landmark = L(index)
+            g.push_back(GenericProjectionFactorCal3_S2(p0, measurement_noise, X(pose_variable_indices[0]), L(index)))
+            g.push_back(GenericProjectionFactorCal3_S2(p1, measurement_noise, X(pose_variable_indices[1]), L(index)))
+
+            point_cloud_hash.index(feature, extra_data = len(point_cloud))
+
+if __name__ == '__main__':
+
+    from data_reading import read_images, load_reference_poses, read_imu, sync_data
+    from visualization import visualize_trajectory
+    from visual_odometry import compute_relative_pose, compute_matches, add_vo_factors
+    from imu_preintegration import add_imu_factors
+
+    imgs = read_images()
+    imu = read_imu()
+    gt_poses = load_reference_poses()
+    imgs_list = []
+    for i, img in enumerate(imgs):
+        if i>1000:
+            break
+        imgs_list.append(img)
+    synced_data = sync_data(imu, imgs_list, gt_poses)
+    initial_values = gtsam.Values()
+    g = NonlinearFactorGraph()
+    keypoint_data = LSHash(
+        hash_size=32,
+        input_dim=128 if USE_SIFT else 32,
+        num_hashtables=1,
+    )
+    g.push_back()
+    add_vo_factors(g, synced_data[0], initial_values, (0,1), keypoint_data)
