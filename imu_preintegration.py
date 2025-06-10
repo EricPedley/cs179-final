9b2f96ce495d1e6a92d3754c80d429870b81d4e8
Lab PC <uavforge.uci.sdp@gmail.com>
2025-06-10 15:27:34 -0700

    figuring out stuff

--- imu_preintegration.py
+++ imu_preintegration.py
@@ -1,7 +1,15 @@
 from data_reading import SyncedDatum
+from gtsam import NonlinearFactorGraph, PreintegrationParams, noiseModel, Vector3
 
 def add_imu_factors(
-    g,
+    g: NonlinearFactorGraph,
     data: SyncedDatum,
     variables: tuple[str,str]
+    """creates factors for how likely are pose estimates based off of IMU readings"""
+    # between pose reading there is a factor
+    # how is a factor represented.
+    # how is the factor function defined.
+
+    # init preintergrator
+    preint = PreintegrationParams
 )
